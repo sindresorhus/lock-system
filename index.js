27719f49fa30416a04acf62ab01239cb78e273dd
Mark Tiedemann <www.marktiedemann@gmail.com>
2017-08-15 00:26:44 +0200

    Add Windows support

--- index.js
+++ index.js
@@ -2,9 +2,18 @@
 const path = require('path');
 const childProcess = require('child_process');
 
+const os = process.platform;
+
 // Binary: https://github.com/sindresorhus/macos-lock
-const BIN = path.join(__dirname, 'lock');
+const DARWIN_BIN = path.join(__dirname, 'lock');
+
+// See: https://superuser.com/questions/21179/command-line-cmd-command-to-lock-a-windows-machine
+const WIN32_CMD = 'rundll32.exe user32.dll,LockWorkStation';
 
 module.exports = () => {
-	childProcess.execFileSync(BIN);
+	switch (os) {
+		case 'darwin': return childProcess.execFileSync(DARWIN_BIN);
+		case 'win32': return childProcess.execSync(WIN32_CMD);
+		default: throw new Error(`Unsupported os: ${os}`);
+	}
 };
