88f8cce651cd8d822b102fb95651bd06457a5e51
Aleksey Kislov <evenfrosty@gmail.com>
2017-08-18 05:27:36 +0300

    Add Linux support (#3)

--- index.js
+++ index.js
@@ -2,6 +2,29 @@
 const path = require('path');
 const childProcess = require('child_process');
 
+const getExistingLinuxCommand = () => {
+	// See: https://askubuntu.com/questions/184728/how-do-i-lock-the-screen-from-a-terminal
+	const commands = [{
+		name: 'xdg-screensaver',
+		arg: 'lock'
+	}, {
+		name: 'gnome-screensaver-command',
+		arg: '--lock'
+	}, {
+		name: 'dm-tool',
+		arg: 'lock'
+	}];
+
+	return commands.find(command => {
+		try {
+			const result = childProcess.execFileSync('which', [command.name], {encoding: 'utf8'});
+			return result && result.length > 0;
+		} catch (err) {
+			return false;
+		}
+	});
+};
+
 module.exports = () => {
 	switch (process.platform) {
 		case 'darwin': {
@@ -12,6 +35,17 @@ module.exports = () => {
 		case 'win32': {
 			// See: https://superuser.com/questions/21179/command-line-cmd-command-to-lock-a-windows-machine
 			childProcess.execFileSync('rundll32.exe', ['user32.dll,LockWorkStation']);
+			break;
+		}
+		case 'linux': {
+			const existingCommand = getExistingLinuxCommand();
+
+			if (existingCommand) {
+				childProcess.execFileSync(existingCommand.name, [existingCommand.arg]);
+			} else {
+				throw new Error('No applicable command found. Please consider installing xdg-screensaver, gnome-screensaver or dm-tool and try again.');
+			}
+
 			break;
 		}
 		default: {
