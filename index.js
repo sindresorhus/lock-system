1471add3d5fe9bf451030767efd60d21be26fa77
Steve Lacy <me@slacy.me>
2017-08-31 09:02:03 -0700

    Add cinnamon-screensaver-command to command error

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ module.exports = () => {
 			if (existingCommand) {
 				childProcess.execFileSync(existingCommand.name, [existingCommand.arg]);
 			} else {
-				throw new Error('No applicable command found. Please consider installing xdg-screensaver, gnome-screensaver, or dm-tool, and try again.');
+				throw new Error('No applicable command found. Please consider installing xdg-screensaver, gnome-screensaver, cinnamon-screensaver-command, or dm-tool, and try again.');
 			}
 
 			break;
